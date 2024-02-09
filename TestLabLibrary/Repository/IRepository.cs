52faac5b5971184168ffeaa6c01c6ab0bbae9ffa
ToanDC28 <toandcse160630@fpt.edu.vn>
2024-02-09 20:37:42 +0700

    Add course and chapter window

---
+++ TestLabLibrary/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestLabEntity.AutoDB;
+using TestLabLibrary.Repository.Question;
+
+namespace TestLabLibrary.Repository
+{
+    public interface IRepository
+    {
+        public AdminRepository AdminRepository { get; }
+        public QuestionRepository QuestionRepository { get; }
+
+        public TlAdmin Admin { get; set; }
+    }
+}
---
+++ TestLabLibrary/Repository/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestLabEntity.AutoDB;
+using TestLabLibrary.Repository.Question;
+
+namespace TestLabLibrary.Repository
+{
+    public class Repository : IRepository
+    {
+        AdminRepository IRepository.AdminRepository => new AdminRepository();
+
+        QuestionRepository IRepository.QuestionRepository => new QuestionRepository();
+
+        TlAdmin IRepository.Admin { get; set; }
+    }
+}
